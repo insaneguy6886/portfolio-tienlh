0b5af883b4359a3cae3b294997168b720909f644
Tien <tienlehung1080@gmail.com>
2023-11-01 22:22:54 +0700

    finish

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+      <div className="container p-12 flex justify-between">
+        <span>Portfolio tienlehung1080</span>
+        <span>
+          <a href="mailto:tienlehung1080@gmail.com">tienlehung1080@gmail.com</a>
+        </span>
+        <span>
+          <a href="tel://0326151311">0326 151 311</a>
+        </span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
